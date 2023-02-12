fdb4d404a5fa377136f25d065c1161f3b4591341
YanaK <Yana.Koval15@yandex.ru>
2023-02-12 22:13:43 +0300

    main

--- src/main/java/ru/netology/sqr/SqrtService.java
+++ src/main/java/ru/netology/sqr/SqrtService.java
@@ -2,12 +2,12 @@ package ru.netology.sqr;
 
 public class SqrtService {
 
-    public int calcSqrt(int l, int h) {
+    public int calcSqrt(int low, int high) {
         int count = 0;
         int sqrt = 0;
         for (int i = 10; i <= 99; i++) {
             sqrt = i * i;
-            if (sqrt >= l && sqrt <= h) {
+            if (sqrt >= low && sqrt <= high) {
                 count = count + 1;
             }
         }
